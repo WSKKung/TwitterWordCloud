3d9a6d6a8b657e86c02d621356044bf9ed5d91ae
WSKKung <armkung493@gmail.com>
2022-11-24 16:40:20 +0700

    Added cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./router/routes.js";
+import cors from "cors"
 
 // Init .env 
 import dotenv from "dotenv"
@@ -7,6 +8,7 @@ dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(router)
 
 app.listen(5000, () => {
