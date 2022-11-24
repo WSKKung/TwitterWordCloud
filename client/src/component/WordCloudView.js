77e150306904713bf827da2cf2a190e0a55111a1
rhyzen5127 <102752650+rhyzen5127@users.noreply.github.com>
2022-11-24 23:35:10 +0700

    Add Picture Icon, Loading, Alert, Image not found png files. Set state for display all images. Change wordcloud font. Fix word cloud frame. Add case status.

--- client/src/component/WordCloudView.js
+++ client/src/component/WordCloudView.js
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactWordcloud from "react-wordcloud";
 import axios from "axios";
-
+import folder from './PictureFolderIcon.png'
+import loader from './Loading.png'
+import Error from './Alert.png'
+import notFound from './ImageNotFound.png'
+import './Loading.css';
 
 
 export default class WordCloudDisplay extends React.Component {
+
+	statuses = {
+		IDLE: "idle",	// ยังไม่ได้ไส่ input
+		LOADING: "loading", // ไส่ input กำลังสร้าง word cloud
+		ERROR: "error", // เกิด error
+		SUCCESS: "sucess", // สร้าง word cloud จาก input สำเร็จ
+		SUCCESS_EMPTY_VALUE: "success_empty_value" // สำเร็จ แต่ word cloud ว่างเปล่า
+	}
 
 	constructor(props) {
 		super(props)
 		this.state = {
 			words: [],
+			status: this.statuses.IDLE,
 			loading: false
 		}
 	}
 
-	wordcloudOptions = {
+	wordcloudRenderOptions = {
 		colors: ["#38aecc", "#0090c1", "#183446", "#046e8f"],
 		enableTooltip: true,
 		deterministic: true,
-		fontFamily: "impact",
+		fontFamily: "impact, Lucida Console, Monospace",
 		fontSizes: [8, 100],
 		fontStyle: "normal",
 		fontWeight: "normal",
@@ -29,7 +42,7 @@ export default class WordCloudDisplay extends React.Component {
 		transitionDuration: 0
 	};
 
-	fetchWordCount = async () => {
+	/*fetchWordCount = async () => {
 		console.log("Fetching for " + this.props.keywords + "... ")
 		if (!this.props.keywords) return
 		this.setState({ loading: true })
@@ -43,32 +56,113 @@ export default class WordCloudDisplay extends React.Component {
 			console.log(err)
 			alert(err.message)
 		})
-		
+	}*/
+
+	fetchWordCount = async () => {
+        console.log("Fetching for " + this.props.keywords + "... ")
+        if (!this.props.keywords) return
+		this.setState({ status: this.statuses.LOADING })
+        this.setState({ loading: true })
+        // request word count from backend server
+        axios.post(
+            "http://localhost:5000/api/word_cloud", 
+            { keywords: this.props.keywords },
+            { headers: { "Content-Type": "multipart/form-data" } }
+        )
+		.then(res => {
+            this.setState({ 
+				words: res.data, 
+				status: res.data.length > 0 ? this.statuses.SUCCESS : this.statuses.SUCCESS_EMPTY_VALUE,
+				loading: false 
+			})
+        })
+		.catch(err => {
+            console.log(err)
+            alert(err.message)
+			// import rhyzen from 'asawaOS'
+			// import { Cat } from 'zoo'
+			// const voraphat = rhyzen()
+			// const mali = new Cat()
+			// voraphat.addChild(mali)
+			// voraphat.sendMessage("Phom ru suk cum")
+            this.setState({
+				words: [], 
+				status: this.statuses.ERROR,
+				loading: false 
+			})
+        })
 	}
 
 	render() {
-		if (this.state.loading) {
-			return (
-				<div style={{
-					display: 'flex',
-					justifyContent: 'center',
-				}}>
-					<div style={{ width: 600, height: 600 }}>
-						Loading...
+
+		switch (this.state.status) {
+
+			// idle
+			case this.statuses.IDLE:
+
+				return (
+					<div style={{ display: 'flex', justifyContent: 'center',}}>
+						<div style={{ width: 600, height: 600, zIndex: 40,display: 'flex',flexDirection:"column",justifyContent: 'center',alignItems: "center"}}>
+							Waiting for input...
+							<img className="img" src={folder}/>
+						</div>
 					</div>
-				</div>
-			)
+				)
+
+			// loading
+			case this.statuses.LOADING:
+
+				return (
+					<div style={{ display: 'flex', justifyContent: 'center',
+					}}>
+						<div style={{ width: 600, height: 600, zIndex: 40,display: 'flex',flexDirection:"column",justifyContent: 'center',alignItems: "center"}}>
+							Loading...
+							<img className="img rotate" src={loader}/>
+						</div>
+					</div>
+				)
+
+			// error
+			case this.statuses.ERROR:
+
+				return (
+					<div style={{ display: 'flex', justifyContent: 'center',
+					}}>
+						<div style={{ width: 600, height: 600, zIndex: 40,display: 'flex',flexDirection:"column",justifyContent: 'center',alignItems: "center"}}>
+							Error!!!
+							<img className="img" src={Error}/>
+						</div>
+					</div>
+				)
+
+			// success
+			case this.statuses.SUCCESS:
+
+				return (
+					<div style={{
+						display: 'flex',
+						justifyContent: 'center',
+					}}>
+						<div className="WordCloudContainer">
+							<ReactWordcloud words={this.state.words} size={[ 580, 580 ]} options={this.wordcloudRenderOptions} />
+						</div>
+					</div>
+				)
+
+			// success but empty word cloud
+			case this.statuses.SUCCESS_EMPTY_VALUE:
+
+				return (
+					<div style={{ display: 'flex', justifyContent: 'center',
+					}}>
+						<div style={{ width: 600, height: 600, zIndex: 40,display: 'flex',flexDirection:"column",justifyContent: 'center',alignItems: "center"}}>
+							{'I cant found any tweets with the specified keywords :^('}
+							<img className="img" src={notFound}/>
+						</div>
+					</div>
+				)
 		}
-		return (
-			<div style={{
-				display: 'flex',
-				justifyContent: 'center',
-			}}>
-				<div style={{ width: 600, height: 600 }}>
-					<ReactWordcloud words={this.state.words} size={[ 600, 600 ]} options={this.wordcloudOptions} />
-				</div>
-			</div>
-		)
+
 	}
 
 	componentDidUpdate(prevProps, prevState) {
